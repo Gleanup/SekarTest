641de17d3a27ff541453a3ef3d094ca49bc62897
sekarkaruppan <45545959+sekarkaruppan@users.noreply.github.com>
2024-02-20 20:29:43 -0500

    Add files via upload

---
+++ SeleniumTest/src/welcometest/Sample.java
@@ -0,0 +1,16 @@
+package welcometest;
+
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("asdfdasfasdf");
+
+		System.setProperty("", "");
+
+		//WebDriver newdriver = new ChromeDriver();
+	
+	}
+
+}
